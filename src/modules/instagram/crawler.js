87691ada38073cd696136e24b3716e13384d79d8
Billy Yang <mz.yyam@gmail.com>
2018-08-27 23:23:32 +0800

    ins爬虫 解析到列表

--- src/modules/instagram/crawler.js
+++ src/modules/instagram/crawler.js
@@ -1,6 +1,31 @@
+import cheerio from 'cheerio';
+import { get as getField } from 'lodash';
+
 import { get } from '../util/request';
+
+/**
+ * 把script标签转成json对象
+ * 通过eval方法，直接执行这段赋值语句
+ */
+const script2JSON = script => {
+  let window = {};
+  eval(script);
+  return window._sharedData;
+};
+
+const handlePage = async profile => {
+  const posts = profile.edges;
+  for (let { node: post } of posts) {
+    console.log('=====post', post);
+  }
+};
 
 export default async (username) => {
   const page = await get(`https://www.instagram.com/${username}`);
-  console.log('====page', page);
+  const $ = cheerio.load(page.data);
+  const script = $('span#react-root').next().html();
+  const data = script2JSON(script);
+  const profile = getField(data, 'entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media');
+  await handlePage(profile);
+  console.log('=====profile', profile.count, profile.edges.length);
 };
